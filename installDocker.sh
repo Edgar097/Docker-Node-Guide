83a67ad6570962706f0320bfd9a7e478e4c64b2e
Edgar Parra <76976401+Edgar097@users.noreply.github.com>
2023-01-07 20:40:23 -0600

    Create installDocker.sh

---
+++ installDocker.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+sudo apt-get update
+sudo apt-get -y install \
+apt-transport-https \
+ca-certificates \
+curl \
+gnupg-agent \
+software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+"deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+$(lsb_release -cs) \
+stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce=5:18.09.5~3-0~ubuntu-bionic docker-ce-cli=5:18.09.5~3-0~ubuntu-bionic containerd.io
+sudo usermod -a -G docker cloud_user
